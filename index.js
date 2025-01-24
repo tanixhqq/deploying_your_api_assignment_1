6dc5ae33c3171ef8e35d11470494b35db5f2a4c8
tanishq45632 <tanishqsinghal483@gmail.com>
2025-01-24 15:01:37 +0530

    adde

--- index.js
+++ index.js
@@ -9,6 +9,20 @@ app.use(express.static('static'));
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
+require('dotenv').config();
+
+// Access environment variables
+const config = {
+  apiKey: process.env.API_KEY,
+  serverSecret: process.env.SERVER_SECRET,
+  isKalvian: process.env.IS_KALVIAN === 'true',
+};
+
+// Export the config object
+module.exports = config;
+
+// Log the variables (Optional for testing)
+console.log(config);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
